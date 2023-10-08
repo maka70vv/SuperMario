422a5c25d8044f350bd99e13873769750b58b5e9
Makarov <25.makarovv@gmail.com>
2023-10-08 19:10:06 +0600

    Mushrooms

---
+++ core/src/com/mygdx/game/Sprites/Items/Mushroom.java
@@ -0,0 +1,53 @@
+package com.mygdx.game.Sprites.Items;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Screens.PlayScreen;
+
+public class Mushroom extends Item {
+    @Override
+    public void defineItem() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(getX(), getY());
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(6 / MyGdxGame.PPM);
+        fdef.filter.categoryBits = MyGdxGame.ITEM_BIT;
+        fdef.filter.maskBits = MyGdxGame.MARIO_BIT |
+                MyGdxGame.OBJECT_BIT |
+                MyGdxGame.GROUND_BIT |
+                MyGdxGame.COIN_BIT |
+                MyGdxGame.BRICK_BIT
+        ;
+
+        fdef.shape = shape;
+        body.createFixture(fdef).setUserData(this);
+    }
+
+    @Override
+    public void use() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+        super.update(dt);
+//        setPosition(body.getPosition().x  - getWidth()/2, body.getPosition().y - getHeight()/2);
+        setCenter(body.getPosition().x,body.getPosition().y);
+        velocity.y = body.getLinearVelocity().y;
+        body.setLinearVelocity(velocity);
+
+    }
+
+    public Mushroom(PlayScreen screen, float x, float y) {
+        super(screen, x, y);
+        setRegion(screen.getAtlas().findRegion("mushroom"), 0, 0, 16, 16);
+        velocity = new Vector2(0.7f, 0);
+    }
+}
